d555c80280f836727391ad7049ba0583fbabe3fd
hyenait <74663327+hyenait@users.noreply.github.com>
2020-11-27 15:41:37 +0600

    My CV

---
+++ files/JavaScript/index.js
@@ -0,0 +1,18 @@
+// typed animation
+var typed = new Typed('.typed', {
+  strings: ['Web Developer', 'Web Designer', 'Freelancer'],
+  typeSpeed: 75,
+  backSpeed: 75,
+  loop: true,
+});
+
+var typed = new Typed('.typeded', {
+  strings: ['Web Developer', 'Web Designer', 'Freelancer'],
+  typeSpeed: 75,
+  backSpeed: 75,
+  loop: true,
+});
+
+// Animation On Scroll
+
+AOS.init();
