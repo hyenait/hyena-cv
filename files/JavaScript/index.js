6414ca0c0e1be660669dfbe0c701cc5fd07f6149
hyenait <74663327+hyenait@users.noreply.github.com>
2020-12-02 12:48:02 +0600

    Update with link

--- files/JavaScript/index.js
+++ files/JavaScript/index.js
@@ -16,3 +16,30 @@ var typed = new Typed('.typeded', {
 // Animation On Scroll
 
 AOS.init();
+
+// Contact Me area
+// click contact me button
+$("#con-btn").click(function () {
+  var input1 = $("#con-name").val();
+  var input2 = $("#con-mail").val();
+  var input3 = $("#con-msg").val();
+
+  if (input1 < 5) {
+    alert("valid Name");
+
+    return false;
+  }
+
+  if (input2 < 10) {
+    alert("Enter Mail")
+    return false;
+  }
+
+  if (input3 < 45) {
+    alert("Enter your Message")
+    return false;
+  }
+
+  return alert("Thanks")
+
+});
